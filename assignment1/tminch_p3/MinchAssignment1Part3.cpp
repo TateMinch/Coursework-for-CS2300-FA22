280fcf330b3fd807d1971eca7389044c7849fd14
Tate Minch <tminch@uccs.edu>
2022-09-08 13:32:48 -0600

    Removed unused variable from part 2 and adding files for part 3

--- assignment1/tminch_p3/MinchAssignment1Part3.cpp
+++ assignment1/tminch_p3/MinchAssignment1Part3.cpp
@@ -35,19 +35,46 @@ int main(int argc, char *argv[]){
     string outputFileName = argv[3];
     vector<vector<double> > file1 = readMatrix(argv[1]);
     vector<vector<double> > file2 = readMatrix(argv[2]);
-    if (file1.size() != file2.size()){
-        cout << "Error! Matrices are not the same dimensions and cannot be added!" << endl;
+    
+    if(file1.size() == 0 || file2.size() == 0){
+        cout << "Error! One or both of the matrix files was empty! Try again!" << endl;
         return 0;
+    }else if (file1[0].size() != file2.size()){
+        cout << "Error! Matrix 1 cols and matrix 2 rows are not the same. Try again!" << endl;
+        return 0;
+    }
+    
+    int finalRows = file1.size();
+    int finalCols = file2[0].size();
+    double** final = new double*[finalRows];
+    for(int i = 0; i < file1.size();i++)
+        final[i] = new double[finalCols];
+
+    for(int i = 0; i < finalRows; i++){
+        for(int j = 0; j < finalCols; j++)
+            final[i][j] = 0;
+    }
+
+    for(int i = 0; i < finalRows; i++){
+        for(int j = 0; j < finalCols; j++){
+            for(int k = 0; k < file1[0].size(); k++){
+                final[i][j] += file1[i][k] * file2[k][j];
+            }
+        }
     }
 
     ofstream outfile;
     outfile.open(outputFileName);
 
-    for(int i = 0; i < file1.size(); i++){
-        for(int j = 0; j < file1[i].size();j++)
-            outfile << file1[i][j] + file2[i][j] << " ";
+    for(int i = 0; i < finalRows; i++){
+        for(int j = 0; j < finalCols;j++)
+            outfile << final[i][j] + final[i][j] << " ";
         outfile << "\n";
     }
+
+    for(int i = 0; i < finalRows; i++)
+        delete [] final[i];
+    delete [] final;
 
     return 0;
 }
